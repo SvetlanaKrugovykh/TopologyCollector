ada077a260c1758fe0f7c19bfeb433b7ccc88979
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-08-03 13:31:22 +0200

    test

--- test-dlink-collection.js
+++ test-dlink-collection.js
@@ -246,7 +246,7 @@ async function testDLinkDataCollection() {
       ],
       paginationInput: "a", // D-Link ждет 'a' для показа всего
       commands: {
-        config: [],
+        config: ["show ver"],
         mac: ["show fdb"]
       },
       description: "D-Link_Switch_212"
